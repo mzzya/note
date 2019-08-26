3d8d07134221196d2a72678c2a985f25dda04df9
hellojqk <657176605@qq.com>
2019-08-26 09:21:51 +0800

    cfssl测试

--- example/tls_ca/generate.sh
+++ example/tls_ca/generate.sh
@@ -4,6 +4,6 @@ openssl genrsa -out ca.key 2048
 
 # 默认-----BEGIN CERTIFICATE REQUEST-----
 openssl req -new  -key ca.key -out ca.pem
-openssl req -new  -key ca.key -out ca.csr
 
-openssl x509 -req -in ca.pem -days 9999 -singkey=ca.key --out ca.crt
+
+openssl x509 -req -in ca.pem -days 9999 -signkey ca.key -out ca.crt
