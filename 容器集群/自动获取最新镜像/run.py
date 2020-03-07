03de1d48b256c20d7e33b43ffb7e0ab87096dc86
wangyang <657176605@qq.com>
2020-03-07 14:50:50 +0800

    获取hub.docker.com上最新镜像脚本

---
+++ 容器集群/自动获取最新镜像/run.py
@@ -0,0 +1,96 @@
+# -*- coding: UTF-8 -*-
+
+import requests
+import re
+import subprocess
+import os
+
+# 想要获取的镜像
+imageNames = ["mysql", "redis", "mongo",
+              "busybox", "alpine", "hellojqk/alpine", "prom/prometheus",
+              "jaegertracing/all-in-one", "grafana/grafana", "prom/prometheus", "nginx", "gitlab/gitlab-ce",
+              "jenkins/jenkins", "docker", "rabbitmq", "kafka", "logstash", "kibana", "elasticsearch", "jenkins"]
+
+# 要匹配的版本号正则 纯v数字版本号
+patterns = ["^v\d+\.\d+\.\d+$", "^\d+\.\d+\.\d+$",
+            "^v\d+\.\d+$", "^\d+\.\d+$", "^\d+$", "^v\d+$"]
+
+# 获取镜像列表url
+url = "https://hub.docker.com/v2/repositories/library/{}/tags/?page_size=50&page=1"
+
+imagesMap = {}
+
+# 遍历镜像列表
+for imageName in imageNames:
+    # 获取结果
+    resp = requests.get(url.format(imageName))
+    results = resp.json()["results"]
+
+    # 最大版本号
+    maxTag = ""
+    # 版本号列表
+    tags = []
+    for item in results:
+        name = item["name"]
+        # 循环匹配版本号
+        for pattern in patterns:
+            r = re.search(pattern, name, re.I)
+            if r != None:
+                tags.append(name)
+                break
+
+    if len(tags) == 0:
+        # print("{}没有匹配的版本号请检查正则".format(imageName))
+        pass
+    elif len(tags) == 1:
+        maxTag = tags[0]
+    else:
+        # 第一位最大的版本号数字
+        # 第二位最大的版本号数字
+        # 第三位组大的版本号数字
+        firstMaxNumber = -1
+        secondMaxNumber = -1
+        threeMaxNumber = -1
+        for i in range(len(tags)):
+            tagStrs = str.split(tags[i], ".")
+            if tagStrs != None:
+                # 先比较第一个版本号数字
+                tagStrs[0] = tagStrs[0].replace("v", "")
+                tagStrs[0] = tagStrs[0].replace("V", "")
+                first = int(tagStrs[0])
+                second = -1
+                three = -1
+                if len(tagStrs) > 1:
+                    second = int(tagStrs[1])
+                if len(tagStrs) > 2:
+                    three = int(tagStrs[2])
+
+                if first < firstMaxNumber:
+                    continue
+                elif first == firstMaxNumber:
+                    # 如果第一个版本号数字相同则开始比较第二个版本数字
+                    if second < secondMaxNumber:
+                        continue
+                    elif second == secondMaxNumber:
+                        if three < threeMaxNumber:
+                            continue
+                        else:
+                            threeMaxNumber = three
+                            maxTag = tags[i]
+                    else:
+                        secondMaxNumber = second
+                        threeMaxNumber = three
+                        maxTag = tags[i]
+                else:
+                    firstMaxNumber = first
+                    secondMaxNumber = second
+                    threeMaxNumber = three
+                    maxTag = tags[i]
+    if maxTag == "":
+        print("{}未匹配到标签".format(imageName))
+        continue
+    imagesMap[imageName] = maxTag
+    # 获取镜像
+    getCmd = "docker pull {}:{}".format(imageName, maxTag)
+    print(getCmd)
+    os.system(getCmd)
