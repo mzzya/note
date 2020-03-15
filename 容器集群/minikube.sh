6bb5e3b7913fa939dc56b01269dc93c31c11e8f3
wangyang <657176605@qq.com>
2020-03-15 09:16:32 +0800

    minikube启动脚本整理

--- 容器集群/minikube.sh
+++ 容器集群/minikube.sh
@@ -1,5 +1,9 @@
-minikube start --extra-config=kubelet.authentication-token-webhook=true --vm-driver=hyperkit --memory=16384 --cpus=4 --registry-mirror=https://o40mvhma.mirror.aliyuncs.com --image-repository=registry.cn-hangzhou.aliyuncs.com/google_containers --alsologtostderr
-minikube start --memory=16384 --cpus=4 --registry-mirror=https://o40mvhma.mirror.aliyuncs.com --image-repository=registry.cn-hangzhou.aliyuncs.com/google_containers --alsologtostderr
-minikube start --memory=16384 --cpus=4 --iso-url='https://kubernetes.oss-cn-hangzhou.aliyuncs.com/minikube/iso/minikube-v1.7.3.iso' --image-mirror-country cn --alsologtostderr
-
-# https://storage.googleapis.com/minikube-preloaded-volume-tarballs/preloaded-images-k8s-v1-v1.17.3-docker-overlay2.tar.lz4
+minikube start \
+--extra-config=kubelet.authentication-token-webhook=true \
+--vm-driver=hyperkit \
+--memory=16384 --cpus=4 \
+--iso-url='https://kubernetes.oss-cn-hangzhou.aliyuncs.com/minikube/iso/minikube-v1.8.0.iso' \
+--registry-mirror=https://o40mvhma.mirror.aliyuncs.com \
+--image-repository=registry.cn-hangzhou.aliyuncs.com/google_containers \
+--kubernetes-version='1.17.4' \
+--alsologtostderr
