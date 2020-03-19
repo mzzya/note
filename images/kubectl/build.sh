af49487e25b61ffe2ec0f2e0d6692a3614feb9f4
wangyang <657176605@qq.com>
2020-03-19 18:20:35 +0800

    kubectl 镜像本地下载并COPY到镜像中

--- images/kubectl/build.sh
+++ images/kubectl/build.sh
@@ -8,12 +8,19 @@ cp ./Template Dockerfile
 
 sed -i "s!\$BASE_IMAGE!hellojqk/${base_image}!g" Dockerfile
 
+# echo "curl -LO https://storage.googleapis.com/kubernetes-release/release/${kubectl_version}/bin/linux/amd64/kubectl"
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/${kubectl_version}/bin/linux/amd64/kubectl
+
+chmod u+x kubectl
+
 #改造后镜像TAG
 image_full_name=hellojqk/kubectl:${kubectl_version}
 
 # alpine 加速 https://blog.csdn.net/freeking101/article/details/80795752
-docker build --no-cache --build-arg kubectl_version=${kubectl_version} -t ${image_full_name} .
+docker build --no-cache -t ${image_full_name} .
 
 docker push ${image_full_name}
 
+# rm kubectl
 rm Dockerfile
