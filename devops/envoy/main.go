65d5dce8d731b79541c3be4a0eee302e1fbe38bd
hellojqk <657176605@qq.com>
2020-07-09 01:04:22 +0800

    配置测试

--- devops/envoy/main.go
+++ devops/envoy/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+var port *int = flag.Int("port", 1234, "help message for flagname")
+
 func main() {
+	flag.Parse()
 	g := gin.Default()
 	g.GET("/", func(c *gin.Context) {
 		header := c.Request.Header
@@ -16,5 +21,5 @@ func main() {
 		headerBts, _ := json.Marshal(header)
 		c.String(http.StatusOK, "%s", headerBts)
 	})
-	g.Run(":1234")
+	g.Run(fmt.Sprintf(":%d", *port))
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/gin-gonic/gin v1.6.3 // indirect
 	github.com/prometheus/client_golang v1.7.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 )
