56d278c03f9d5e4e4307209d95bf915273db9012
wangyang <wangyang@hellojqk.lan>
2020-07-23 00:15:27 +0800

    暂存

--- devops/k8s/webhook/main.go
+++ devops/k8s/webhook/main.go
@@ -4,12 +4,24 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"path/filepath"
 
+	"github.com/gin-gonic/gin"
+	"k8s.io/api/admission/v1beta1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
+)
+
+var (
+	runtimeScheme = runtime.NewScheme()
+	codecs        = serializer.NewCodecFactory(runtimeScheme)
+	deserializer  = codecs.UniversalDeserializer()
+	defaulter     = runtime.ObjectDefaulter(runtimeScheme)
 )
 
 func main() {
@@ -36,4 +48,17 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("%#v", nsList)
+}
+
+func g() {
+	g := gin.New()
+	//https://github.com/yaoice/webhook-demo/blob/master/pkg/webhook/webhook.go
+	g.GET("/validate", func(c *gin.Context) {
+		body, _ := ioutil.ReadAll(c.Request.Body)
+		requestedAdmissionReview := v1beta1.AdmissionReview{}
+
+		deserializer := codecs.UniversalDeserializer()
+		deserializer.Decode(body, nil, &requestedAdmissionReview)
+
+	})
 }
