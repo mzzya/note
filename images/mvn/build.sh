c8666578aa63d6082ac27e87606e0cbe61da3f2f
汪洋 <wangyang@colipu.com>
2020-08-04 12:30:13 +0800

    添加 mvn基础镜像

---
+++ images/mvn/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#基础镜像
+base_image="maven:3.6.3-openjdk-8"
+
+cp ./Template Dockerfile
+
+sed -i "s!\$BASE_IMAGE!${base_image}!g" Dockerfile
+
+#改造后镜像TAG
+image_full_name=hellojqk/${base_image}
+
+# alpine 加速 https://blog.csdn.net/freeking101/article/details/80795752
+docker build --no-cache -t ${image_full_name} .
+
+docker push ${image_full_name}
+
+# rm Dockerfile
