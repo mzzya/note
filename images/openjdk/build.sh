58dfc4dbde10905d26c27571c7613e8a9d75c768
wangyang <657176605@qq.com>
2021-03-10 11:06:05 +0800

    openjdk基于官方openjdk debian buster调整

--- images/openjdk/build.sh
+++ images/openjdk/build.sh
@@ -2,16 +2,16 @@
 
 #基础镜像
 # base_image="hellojqk/alpine:3.12.3"
-base_image="adoptopenjdk/openjdk8:jdk8u252-b09-alpine"
+base_image="openjdk:8u282-buster"
 
 cp ./Template Dockerfile
 
 sed -i "s!\$BASE_IMAGE!${base_image}!g" Dockerfile
 
-version="jdk8u252-b09-alpine"
+version="8u282-buster"
 
 #改造后镜像TAG
-image_full_name="hellojqk/adoptopenjdk8:${version}"
+image_full_name="hellojqk/openjdk:${version}"
 
 # alpine 加速 https://blog.csdn.net/freeking101/article/details/80795752
 docker build --no-cache --build-arg kubectl_version=${version} -t ${image_full_name} .
