49c845cbf91d1335e0c4549327404bbcb42d7c62
wangyang <657176605@qq.com>
2021-03-30 13:10:40 +0800

    ++

--- 规则引擎/start_business-central-wb.sh
+++ 规则引擎/start_business-central-wb.sh
@@ -2,5 +2,6 @@
 
 # Start Wildfly with the given arguments.
 echo "Running business-central workbench on JBoss Wildfly..."
+# exec ./standalone.sh -b $JBOSS_BIND_ADDRESS -c $KIE_SERVER_PROFILE.xml -Dorg.kie.demo=$KIE_DEMO -Dorg.kie.example=$KIE_DEMO -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true
 exec ./standalone.sh -b $JBOSS_BIND_ADDRESS -bmanagement $JBOSS_BIND_ADDRESS -c $KIE_SERVER_PROFILE.xml -Dorg.kie.demo=$KIE_DEMO -Dorg.kie.example=$KIE_DEMO -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true
 exit $?
